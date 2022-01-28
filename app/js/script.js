f16a9b808405ce9877e34e66208aeea5558c5bac
jcaltamia <jayceealtamia@gmail.com>
2022-01-28 21:45:14 +0800

    fix slider

---
+++ app/js/script.js
@@ -0,0 +1,10 @@
+var output = document.getElementById('data-left');
+var slider = (document.getElementById('range-slider').oninput = function () {
+  var value = ((this.value - this.min) / (this.max - this.min)) * 100;
+  this.style.background =
+    'linear-gradient(to right, hsl(6, 100%, 80%) 0%, hsl(335, 100%, 65%) ' +
+    value +
+    '%,  hsl(229, 57%, 11%) 0%, hsl(229, 57%, 11%) 100%)';
+
+  output.innerHTML = this.value;
+});
